ac4a6c2a37af9c98e0742741712b6555e850746f
Devin Miller <41914490+howthegodschill@users.noreply.github.com>
2022-07-19 09:03:06 -0400

    Command Formatting Fixes
    1. Spaced commands from one another.
    2. Adjusted formatting of the Nessus link command for better
    readability.

--- nessus_relink.sh
+++ nessus_relink.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-###################################################################################
-# PURPOSE - To unlink the Nessus agent , then re-link with a new Tenable instance #
-###################################################################################
+####################################################
+# PURPOSE - To unlink then relink the Nessus agent #
+####################################################
 
-##########################################################
-# Gathers the Nessus group and places it into a variable #
-##########################################################
+# Gathers the Nessus group and places it into a variable
 group=$(/Library/NessusAgent/run/sbin/nessuscli fix --secure --list | grep -io 'GROUP_VARIABLE' | tr -d ' ')
 
-###########################################
-# Unlinks, then re-links the Nessus Agent #
-###########################################
+# Unlinks the Nessus Agent
 /Library/NessusAgent/run/sbin/nessuscli agent unlink --force
+
 sleep 5
-/Library/NessusAgent/run/sbin/nessuscli agent link --key=<INSERT NESSUS KEY> --host=<INSERT NESSUS HOST> --port=<INSERT NESSUS PORT> --groups=$group
+
+# Links the Nessus Agent to the proper group in the Tenable instance
+/Library/NessusAgent/run/sbin/nessuscli agent link \
+--key=<INSERT_NESSUS_KEY> \
+--host=<INSERT_NESSUS_HOST> \
+--port=<INSERT_NESSUS_PORT> \
+--groups=$group
 
 exit 0
